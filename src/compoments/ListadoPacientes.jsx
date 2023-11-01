bf5a2de818a89658da4f0bb146d0e7df96ba7af1
unknown <angelpalacios01234@gmail.com>
2023-11-01 19:24:35 -0300

    [ADD]: framer motion

--- src/compoments/ListadoPacientes.jsx
+++ src/compoments/ListadoPacientes.jsx
@@ -1,22 +1,34 @@
 import { Paciente } from './Paciente';  // Asegúrate de importar el componente Paciente
+import { motion } from "framer-motion";
 
 export const ListadoPacientes = ({ pacientes, eliminarPaciente }) => {
   return (    
     <div className="w-full lg:w-3/4 p-4">
       <h2 className="text-2xl font-bold mb-4">Listado de Pacientes</h2>
-      <ul className="bg-white rounded-lg shadow-md p-4">
+      <motion.ul
+        className="bg-white rounded-lg shadow-md p-4"
+        initial={{ opacity: 0, y: 20 }} // Estado inicial
+        animate={{ opacity: 1, y: 0 }} // Animación de entrada
+        exit={{ opacity: 0, y: -20 }} // Animación de salida
+      >
         {pacientes.map((paciente, index) => (
-          <Paciente
+          <motion.li
             key={index}
-            nombre={paciente.nombre}
-            apellido={paciente.apellido}
-            telefono={paciente.telefono}
-            fecha={paciente.fecha}
-            sintomas={paciente.sintomas}
-            onEliminar={() => eliminarPaciente(index)} // Llama a la función de eliminación con el índice del paciente
-          />
+            initial={{ opacity: 0, x: -20 }} // Estado inicial
+            animate={{ opacity: 1, x: 0 }} // Animación de entrada
+            exit={{ opacity: 0, x: 20 }} // Animación de salida
+          >
+            <Paciente
+              nombre={paciente.nombre}
+              apellido={paciente.apellido}
+              telefono={paciente.telefono}
+              fecha={paciente.fecha}
+              sintomas={paciente.sintomas}
+              onEliminar={() => eliminarPaciente(index)}
+            />
+          </motion.li>
         ))}
-      </ul>
+      </motion.ul>
     </div>
   );
 }
